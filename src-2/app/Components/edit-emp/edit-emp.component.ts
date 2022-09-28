071ac349c0c2703986e6cf915bb22d1710448941
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-09-28 16:53:09 +0530

    Add files via upload

--- src-2/app/Components/edit-emp/edit-emp.component.ts
+++ src-2/app/Components/edit-emp/edit-emp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/Models/employee';
+import { EmployeeService } from 'src/app/Services/employee.service';
 
 @Component({
   selector: 'app-edit-emp',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-emp.component.css']
 })
 export class EditEmpComponent implements OnInit {
+  foundEmp : Employee = {} as Employee;
+  empId : any;
+  constructor(private empService : EmployeeService, private activatedRoute : ActivatedRoute) { }
 
-  constructor() { }
-
+  //Find the matching record and display it in the View
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((map)=>{
+      this.empId = map.get("id");
+      this.empService.getEmployee(parseInt(this.empId)).subscribe((data : Employee)=>{
+        this.foundEmp = data;
+      })
+    })
   }
 
+  //Event handler for the Submit button that is clicked after the changes are made by the user.
+  onSubmit(): void{
+    this.empService.updateEmployee(this.foundEmp).subscribe((data)=>{
+      alert(data);
+    })
+  }
 }
--- src-2/app/Components/view-emp/view-emp.component.ts
+++ src-2/app/Components/view-emp/view-emp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Employee } from 'src/app/Models/employee';
+import { EmployeeService } from 'src/app/Services/employee.service';
 
 @Component({
   selector: 'app-view-emp',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-emp.component.css']
 })
 export class ViewEmpComponent implements OnInit {
-
-  constructor() { }
+  //data of the Component...
+  empId : any;
+  selected : Employee = {} as Employee
+  constructor(private empService : EmployeeService, private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+    //get the parameters from the Url using paramMap function of the activatedRoute. 
+    this.activatedRoute.paramMap.subscribe((map)=>{
+      //Get the id from the Map(Map is JS's version of dictionary)
+      this.empId = map.get("id");//Get the value of the key called id....
+      if(this.empId != null){//if not null call the service method....
+        this.empService.getEmployee(parseInt(this.empId)).subscribe((data)=>{
+          this.selected = data as Employee;//set the foundEmp object to the member. 
+        })
+      }
+    })
   }
-
 }
