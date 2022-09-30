6f0dca4ca9e72c3a0b051c6a1d52ac7b4b23ccc1
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-09-30 09:17:08 +0530

    Add files via upload

---
+++ MvcCore Demos/EmployeeWebApi/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApi.Controllers
+{
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private MyDbContext _context = null;
+
+        public EmployeeController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("Employees/All")]
+        public List<Employee> GetAll() => _context.Employees.ToList();
+
+        [Route("Employees/{id}")]
+        public Employee GetEmp(int id) => _context.Employees.FirstOrDefault((e) => e.EmpId == id);
+
+        [Route("Employees")]
+        [HttpPost]
+        public string AddNew(Employee emp)
+        {
+            _context.Employees.Add(emp);
+            _context.SaveChanges();
+            return "Employee added to the server";  
+        }
+
+        [Route("Employees")]
+        [HttpPut]
+        public string Update(Employee emp)
+        {
+            var selected = _context.Employees.FirstOrDefault((e) => e.EmpId == emp.EmpId);
+            if (selected == null) return "Employee not found to update";
+            selected.EmpName = emp.EmpName;
+            selected.EmpSalary = emp.EmpSalary;
+            selected.EmpAddress = emp.EmpAddress;
+            selected.EmpImage = emp.EmpImage;
+            _context.SaveChanges();
+            return "Employee updated in the server";
+        }
+
+        [Route("Employees/{id}")]
+        [HttpDelete]
+        public string Delete(int id)
+        {
+            var foundRec = _context.Employees.FirstOrDefault((e) => e.EmpId == id);
+            if (foundRec == null) return "Failed to delete as no record found";
+            _context.Employees.Remove(foundRec);
+            _context.SaveChanges();
+            return "Employee deleted successfully";
+        }
+    }
+}
---
+++ MvcCore Demos/EmployeeWebApi/Models/DataModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApi.Models
+{
+    [Table("EmployeeTable")]
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public double EmpSalary { get; set; }
+        public string EmpImage { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee { EmpId = 111, EmpAddress ="Bangalore", EmpName ="Phaniraj", EmpSalary = 56000 },
+                new Employee { EmpId = 112, EmpAddress = "Bangalore", EmpName = "Govardhan", EmpSalary = 36000 }
+            );
+        }
+    }
+}
