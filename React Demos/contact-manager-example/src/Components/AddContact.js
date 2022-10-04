6e5e1188dc9158246ff014dbac0c145e9e3599ea
Phaniraj <79626160+phani-training@users.noreply.github.com>
2022-10-04 15:57:20 +0530

    Add files via upload

--- React Demos/contact-manager-example/src/Components/AddContact.js
+++ React Demos/contact-manager-example/src/Components/AddContact.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { ContactService } from "../Services/ContactService";
+import { useNavigate } from 'react-router-dom'
 
 let AddContact = ()=> {
+    let navigate = useNavigate();
     let [state, setState] = useState({
         contact : {
             "id": 0,
@@ -10,8 +13,26 @@ let AddContact = ()=> {
             "photo": ""
         }
     })
+    let updateInput = (event)=>{
+        setState({
+            ...state, 
+            contact : {
+                ...state.contact, 
+                [event.target.name] : event.target.value
+            }
+        })
+    }
 
     let { contact } = state;
+
+    let onSubmit = (event)=>{
+        event.preventDefault();
+        ContactService.addContact(contact).then((res)=>{
+            alert("Added successfully");
+            navigate('/contacts/list', { replace :true});
+        })
+
+    }
     ///////////////////////////////////////////V-DOM//////////////////////////////////
  return(
     <React.Fragment>
@@ -21,24 +42,26 @@ let AddContact = ()=> {
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
+                    <form onSubmit={onSubmit}>
                     <p>
-                        <input name="id" value={contact.id} className="form-control" placeholder="Enter the Id"/>
+                        <input name="id" value={contact.id} onChange={updateInput} className="form-control" placeholder="Enter the Id"/>
                     </p>
                     <p>
-                        <input className="form-control" placeholder="Enter the Contact Name"/>
+                        <input onChange={updateInput} className="form-control" value={contact.name} name="name" placeholder="Enter the Contact Name"/>
                     </p>
                     <p>
-                        <input className="form-control" placeholder="Enter the EMail"/>
+                        <input onChange={updateInput} className="form-control" value={contact.email} name="email" placeholder="Enter the EMail"/>
                     </p>
                     <p>
-                        <input className="form-control" placeholder="Enter the Contact No"/>
+                        <input onChange={updateInput} className="form-control" value={contact.contactNo} name="contactNo" placeholder="Enter the Contact No"/>
                     </p>
                     <p>
-                        <input className="form-control" placeholder="Enter the FileName"/>
+                        <input onChange={updateInput} className="form-control" value={contact.photo} name="photo" placeholder="Enter the FileName"/>
                     </p>
                     <p>
                         <button className="btn btn-success">Save Record</button>
                     </p>
+                    </form>
                 </div>
             </div>
         </div>
